8b87907b04959477cf546eb7fdd815680f1a38f5
GambiTemp <alessiogambi@users.noreply.github.com>
2024-05-17 19:02:11 +0200

    Basic setup

---
+++ tests/test_public_tests.py
@@ -0,0 +1,6 @@
+# Import your implementatino in the public tests
+from traits.implementation import Traits
+
+def test_smoke_test():
+    """ Simply check that the test can be run from the root of the project with pytest public """
+    pass
--- traits/interface.py
+++ traits/interface.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-class AbstractAdminTraits(ABC):
+class TraitsInterface(ABC):
     """
     This is the reference (abstract) class that defines the main admin features. Admin features cannot be invoked by
     regular (non-admin) database users!
@@ -44,27 +44,4 @@ class AbstractAdminTraits(ABC):
 
     @abstractmethod
     def add_trains():
-        pass
-
-
-class AbstractTraits(ABC):
-    """
-    This is the reference (abstract) class to implement for storing and retrieving the data fromn the TRAITS database.
-    """
-
-    #     View Trains Schedule
-    # Search Trains
-    # Check Seats Availability
-    # Train Timings
-    # Fare Enquiry
-    # Trains Between Stations
-    # Booking Seats Online
-    # Login and Logout Security
-    # Password Changes
-    # Payment Gateway
-    # Ticket Booking History
-
-    @abstractmethod
-    def search_for_connection(starting_station: str, ending_station:str, options: dict) -> list:
-
         pass
